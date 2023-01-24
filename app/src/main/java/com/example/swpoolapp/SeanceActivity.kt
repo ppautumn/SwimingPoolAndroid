8562b977ddf27fd415df4c49724110fa31307768
Dimitry <Kholod.ural@gmail.com>
2023-01-24 19:20:12 +0500

    take info about seance

--- app/src/main/java/com/example/swpoolapp/SeanceActivity.kt
+++ app/src/main/java/com/example/swpoolapp/SeanceActivity.kt
@@ -1,11 +1,16 @@
 package com.example.swpoolapp
 
+import android.content.Context
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import java.io.FileNotFoundException
+import java.net.HttpURLConnection
+import java.net.URL
 
 class SeanceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,15 +18,62 @@ class SeanceActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seance)
 
+        val tokensStorage =
+            getSharedPreferences("tokens", Context.MODE_PRIVATE)
+        val token = tokensStorage.getString("accessToken", "")
+
+        var answ = ArrayList<String>()
+        val x = object : Thread() {
+            override fun run() {
+                println("running from Thread: ${Thread.currentThread()}")
+                val url = "${UsefullData.serverAddr}/upcoming-slots/"
+                Log.e("Seance", "token" + token)
+                answ = sendGet(url, token + "", true)
+                Log.e("Seance", answ[0] + " " + answ[1]);
+            }
+        }
+        x.start()
+        x.join()
+
+
         val seanceLinear = findViewById<LinearLayout>(R.id.seancesLinear1)
-        val tt = findViewById<TextView>(R.id.SeancesTV)
+        val itemView =
+            layoutInflater.inflate(R.layout.fragment_seance, seanceLinear)
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
+        val btCancel = itemView.findViewById<Button>(R.id.btCancel)
+        btCancel.setOnClickListener {
+            val duration = Toast.LENGTH_SHORT
+            val toast = Toast.makeText(applicationContext, "Запись удалена!", duration)
+            toast.show()
+        }
 
-        val frag: Fragment = seanceFragment()
-        fragmentTransaction.add(seanceLinear.id, frag)
-        fragmentTransaction.commit()
+    }
 
+    private fun sendGet(URLReq: String, token: String, auth: Boolean): ArrayList<String> {
+        val urlTxt = URLReq
+        var output = ""
+        val url = URL(urlTxt)
+        val conn = (url.openConnection() as HttpURLConnection).apply {
+            requestMethod = "GET"
+            doInput = true
+        }
+        if (auth)
+            conn.setRequestProperty("Authorization", "Bearer  ${token}")
+
+        conn.connect()
+        val code = conn.responseCode
+        try {
+            output = conn.inputStream.bufferedReader().readText()
+        } catch (e: FileNotFoundException) {
+            var ar = ArrayList<String>()
+            ar.add(code.toString())
+            val errTxt = conn.errorStream.bufferedReader().readText()
+            ar.add(errTxt)
+            return ar
+        }
+        var ar = ArrayList<String>()
+        ar.add(code.toString())
+        ar.add(output)
+        return ar
     }
 }
